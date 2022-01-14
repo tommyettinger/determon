fe7c796e232e98d181d4758d07d1868580e971b6
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-13 22:12:55 -0800

    Start by adding a MathD with constants for now.

---
+++ Determon/MathD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Determon
+{
+    /// <summary>
+    /// Deterministic/decimal math functions, most of them approximations.
+    /// </summary>
+    public class MathD
+    {
+        /// <summary>
+        /// The irrational number pi, to 104 digits as a decimal. It is likely that far fewer digits will actually be used.
+        /// </summary>
+        public const decimal PI = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214M;
+        /// <summary>
+        /// The irrational number e, Euler's totient, to 104 digits as a decimal. It is likely that far fewer digits will actually be used.
+        /// </summary>
+        public const decimal E = 2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746M;
+
+    }
+}

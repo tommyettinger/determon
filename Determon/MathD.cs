fb3df6a925ac9d90b67fd7ac780d5b4f6e90b339
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-13 22:13:43 -0800

    Now something useful! Sqrt.
    Thanks to Stack Overflow user Bobson for this method.

--- Determon/MathD.cs
+++ Determon/MathD.cs
@@ -16,5 +16,34 @@ namespace Determon
         /// </summary>
         public const decimal E = 2.71828182845904523536028747135266249775724709369995957496696762772407663035354759457138217852516642742746M;
 
+        /// <summary>
+        /// A square root method for decimals.
+        /// </summary>
+        /// <param name="d">The square to find the square root of.</param>
+        /// <returns>The exact square root of d, as well as a decimal can represent it.</returns>
+        public static decimal Sqrt(decimal d)
+        {
+            return Sqrt(d, d * 0.5M);
+        }
+
+        /// <summary>
+        /// A variant square root method for decimals that allows specifying an approximate guess, or seed value, that is likely to be close to the actual square root.
+        /// </summary>
+        /// <remarks>
+        /// This has maximum precision. Credit to Bobson for coming up with this recursive method: https://stackoverflow.com/a/13282997/786740
+        /// </remarks>
+        /// <param name="d">The square to find the square root of.</param>
+        /// <param name="guess">A decimal that is hopefully relatively close to the actual square root, used to speed up the algorithm if it is close.</param>
+        /// <returns>The exact square root of d, as well as a decimal can represent it.</returns>
+        public static decimal Sqrt(decimal d, decimal guess)
+        {
+            var result = d / guess;
+            var average = (guess + result) * 0.5M;
+
+            if (average == guess) // This checks for the maximum precision possible with a decimal.
+                return average;
+            else
+                return Sqrt(d, average);
+        }
     }
 }

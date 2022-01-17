7ab76be43157a085da291f9bd255002c724a9299
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-16 23:09:57 -0800

    Start performance testing with trig.
    These methods are way slower than I expected, though they are also very precise.

--- Determon.PerformanceTests/Program.cs
+++ Determon.PerformanceTests/Program.cs
@@ -1,55 +1,45 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System;
+using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using ShaiRandom.Generators;
 
 namespace Determon.PerformanceTests
 {
-//    public class RandomULongBoundedComparison
-//    {
-//
-//        private readonly DistinctRandom _distinctRandom = new DistinctRandom(1UL);
-//        private readonly LaserRandom _laserRandom = new LaserRandom(1UL);
-//        private readonly TricycleRandom _tricycleRandom = new TricycleRandom(1UL);
-//        private readonly FourWheelRandom _fourWheelRandom = new FourWheelRandom(1UL);
-//        private readonly StrangerRandom _strangerRandom = new StrangerRandom(1UL);
-//        private readonly Xoshiro256StarStarRandom _xoshiro256StarStarRandom = new Xoshiro256StarStarRandom(1UL);
-//        private readonly RomuTrioRandom _romuTrioRandom = new RomuTrioRandom(1UL);
-//        private readonly MizuchiRandom _mizuchiRandom = new MizuchiRandom(1UL);
-//
-//#if NET6_0
-//        private readonly System.Random _seededRandom = new System.Random(1);
-//        private readonly System.Random _unseededRandom = new System.Random();
-//
-//        [Benchmark]
-//        public long Seeded() => _seededRandom.NextInt64(1L, 1000L);
-//
-//        [Benchmark]
-//        public long Unseeded() => _unseededRandom.NextInt64(1L, 1000L);
-//#endif
-//
-//        [Benchmark]
-//        public ulong Distinct() => _distinctRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong Laser() => _laserRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong Tricycle() => _tricycleRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong FourWheel() => _fourWheelRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong Stranger() => _strangerRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong XoshiroStarStar() => _xoshiro256StarStarRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong RomuTrio() => _romuTrioRandom.NextULong(1UL, 1000UL);
-//
-//        [Benchmark]
-//        public ulong Mizuchi() => _mizuchiRandom.NextULong(1UL, 1000UL);
-//    }
+    ///<summary>
+    ///|    Method |         Mean |      Error |      StdDev |
+    ///|---------- |-------------:|-----------:|------------:|
+    ///| BaselineM |    69.602 ns |  1.4124 ns |   1.8365 ns |
+    ///|      SinM | 4,217.138 ns | 82.2736 ns | 128.0901 ns |
+    ///|      CosM | 2,526.321 ns | 20.2870 ns |  17.9839 ns |
+    ///|      TanM | 3,414.984 ns | 63.0010 ns |  58.9312 ns |
+    ///| BaselineD |     3.453 ns |  0.0945 ns |   0.1088 ns |
+    ///|      SinD |    17.009 ns |  0.3147 ns |   0.2944 ns |
+    ///|      CosD |    16.910 ns |  0.2236 ns |   0.1867 ns |
+    ///|      TanD |    20.080 ns |  0.3018 ns |   0.2823 ns |
+    ///</summary>
+    public class TrigonometryComparison
+    {
+        private readonly MizuchiRandom _random = new MizuchiRandom(1UL);
+
+        [Benchmark]
+        public decimal BaselineM() => (_random.NextDecimal() * MathD.Tau - MathD.Pi);
+
+        [Benchmark]
+        public decimal SinM() => MathD.Sin(_random.NextDecimal() * MathD.Tau - MathD.Pi);
+        [Benchmark]
+        public decimal CosM() => MathD.Cos(_random.NextDecimal() * MathD.Tau - MathD.Pi);
+        [Benchmark]
+        public decimal TanM() => MathD.Tan(_random.NextDecimal() - 0.5M);
+
+        [Benchmark]
+        public double BaselineD() => (_random.NextDouble() * Math.PI * 2.0 - Math.PI);
+        [Benchmark]
+        public double SinD() => Math.Sin(_random.NextDouble() * Math.PI * 2.0 - Math.PI);
+        [Benchmark]
+        public double CosD() => Math.Cos(_random.NextDouble() * Math.PI * 2.0 - Math.PI);
+        [Benchmark]
+        public double TanD() => Math.Tan(_random.NextDouble() * Math.PI * 2.0 - Math.PI);
+    }
 
     internal static class Program
     {

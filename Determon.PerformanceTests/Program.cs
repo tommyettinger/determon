ea63fcabcd445fa299f8d3320f409c75f6b673e1
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-01-16 23:34:48 -0800

    More benchmarks; Sin() is better now.

--- Determon.PerformanceTests/Program.cs
+++ Determon.PerformanceTests/Program.cs
@@ -6,17 +6,20 @@ using ShaiRandom.Generators;
 namespace Determon.PerformanceTests
 {
     ///<summary>
-    ///|    Method |         Mean |      Error |      StdDev |
-    ///|---------- |-------------:|-----------:|------------:|
-    ///| BaselineM |    69.602 ns |  1.4124 ns |   1.8365 ns |
-    ///|      SinM | 4,217.138 ns | 82.2736 ns | 128.0901 ns |
-    ///|      CosM | 2,526.321 ns | 20.2870 ns |  17.9839 ns |
-    ///|      TanM | 3,414.984 ns | 63.0010 ns |  58.9312 ns |
-    ///| BaselineD |     3.453 ns |  0.0945 ns |   0.1088 ns |
-    ///|      SinD |    17.009 ns |  0.3147 ns |   0.2944 ns |
-    ///|      CosD |    16.910 ns |  0.2236 ns |   0.1867 ns |
-    ///|      TanD |    20.080 ns |  0.3018 ns |   0.2823 ns |
+    ///|    Method |         Mean |      Error |     StdDev |
+    ///|---------- |-------------:|-----------:|-----------:|
+    ///| BaselineM |    68.428 ns |  0.7111 ns |  0.6304 ns |
+    ///|      SinM | 2,334.630 ns | 43.1611 ns | 40.3729 ns |
+    ///|      CosM | 2,562.045 ns | 28.4909 ns | 25.2564 ns |
+    ///|      TanM | 2,989.138 ns | 58.1600 ns | 62.2305 ns |
+    ///| BaselineD |     3.383 ns |  0.0267 ns |  0.0208 ns |
+    ///|      SinD |    17.016 ns |  0.1117 ns |  0.0933 ns |
+    ///|      CosD |    17.141 ns |  0.3321 ns |  0.3107 ns |
+    ///|      TanD |    20.678 ns |  0.4331 ns |  0.4051 ns |
     ///</summary>
+    ///<remarks>
+    /// The timing on the M benchmarks, for decimals, seems inaccurate between runs, but always extremely high.
+    /// </remarks>
     public class TrigonometryComparison
     {
         private readonly MizuchiRandom _random = new MizuchiRandom(1UL);
